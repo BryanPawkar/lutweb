bedc4e77b215642e8392f0f30448ff9ebd0b7aeb
BryanPawkar <bracosmo@gmail.com>
2020-10-23 13:05:03 -0500

    Updates

---
+++ precache-manifest.f98d476608146cea392a20bd7b82ca84.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1d847084e95aff3a8921d055024172",
+    "url": "/lutweb/index.html"
+  },
+  {
+    "revision": "e4b1adefcf42acd8e7ed",
+    "url": "/lutweb/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1ea9b32930084f84dd6b",
+    "url": "/lutweb/static/css/main.bad1ac63.chunk.css"
+  },
+  {
+    "revision": "e4b1adefcf42acd8e7ed",
+    "url": "/lutweb/static/js/2.ac5b92dd.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/lutweb/static/js/2.ac5b92dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea9b32930084f84dd6b",
+    "url": "/lutweb/static/js/main.6320d128.chunk.js"
+  },
+  {
+    "revision": "f8976eb5399a19c53f0f",
+    "url": "/lutweb/static/js/runtime-main.fa0ea005.js"
+  },
+  {
+    "revision": "27bb6d2eea8ade5cf299c302f311af3b",
+    "url": "/lutweb/static/media/Organo.27bb6d2e.svg"
+  },
+  {
+    "revision": "2b30e200e98f6e8609f1c4f2c59e961e",
+    "url": "/lutweb/static/media/Organo.2b30e200.eot"
+  },
+  {
+    "revision": "6ac39723b1e683eeac4febbffe6d3264",
+    "url": "/lutweb/static/media/Organo.6ac39723.woff"
+  },
+  {
+    "revision": "a5e7f9638367a92e585ed99e02c71d3c",
+    "url": "/lutweb/static/media/Organo.a5e7f963.ttf"
+  },
+  {
+    "revision": "660f91489eb296772297fa54565d47fe",
+    "url": "/lutweb/static/media/logo.660f9148.png"
+  },
+  {
+    "revision": "867b85115ee4af1d18a3b767ba55a5d4",
+    "url": "/lutweb/static/media/pika.867b8511.svg"
+  },
+  {
+    "revision": "547e5cbadd2e7ca446e44a2ddfa3c199",
+    "url": "/lutweb/static/media/plan1.547e5cba.jpg"
+  },
+  {
+    "revision": "b3c3d4b2a376b62a74e757c31858a291",
+    "url": "/lutweb/static/media/plan2.b3c3d4b2.jpg"
+  },
+  {
+    "revision": "553c59d12c691a830f9497b44e0dbf73",
+    "url": "/lutweb/static/media/plan3.553c59d1.jpg"
+  },
+  {
+    "revision": "c563b49fcf02ea014a7d492a77cbd7aa",
+    "url": "/lutweb/static/media/plan4.c563b49f.jpg"
+  },
+  {
+    "revision": "4ce99ec65daa1112928f7c8111fc5365",
+    "url": "/lutweb/static/media/video.4ce99ec6.mp4"
+  }
+]);
